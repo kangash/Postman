1b187842d8fbce15d5dcda30fc6f74b2b88c90be
kangash <kangash.aleksei@gmail.com>
2023-05-19 11:04:13 +0300

    added task 1.1 js

---
+++ Task 1/Task1.js
@@ -0,0 +1,9 @@
+﻿// http://162.55.220.72:5005/first
+// 1. Проверить статус код 200
+pm.test("Status code is 200", function () {
+    pm.response.to.have.status(200);
+});
+// 2. Проверить, что в body приходит правильный string
+pm.test("Body matches string", function () {
+    pm.expect(pm.response.text()).to.include("This is the first responce from server!ss");
+});
