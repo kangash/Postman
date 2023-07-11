e6b6afc8dbd90f462b668e544816f0770141f3c9
kangash <kangash.aleksei@gmail.com>
2023-07-11 13:58:22 +0300

    add task 2

--- case_2/case_2.js
+++ case_2/case_2.js
@@ -5,3 +5,15 @@ pm.test('Status code is 200', function () {
 });
 
 // Задание 2. Написать тест, который проверяет добавление случайного комментария случайным пользователем. Тест должен проверить, что ответ 200, на странице присутствует ваш комментарий и анонимный автор.
+
+pm.test('Status code is 200', function () {
+  pm.response.to.have.status(200);
+});
+
+pm.test('Post has a new comment', function () {
+  pm.expect(pm.response.text()).to.include(pm.environment.get('comment'));
+});
+
+pm.test('Comment has a required author', function () {
+  pm.expect(pm.response.text()).to.include(pm.environment.get('author'));
+});
