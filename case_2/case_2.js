4e3bb77969701fa9a11e1f03e1f4c44ee892281d
kangash <kangash.aleksei@gmail.com>
2023-07-11 13:59:46 +0300

    add task 3

--- case_2/case_2.js
+++ case_2/case_2.js
@@ -19,3 +19,15 @@ pm.test('Comment has a required author', function () {
 });
 
 // Задание 3.	Написать тест, который проверяет добавление случайного комментария зарегистрированным пользователем.  Тест должен проверить, что ответ 200, на странице присутствует ваш комментарий и зарегистрированный автор.
+
+pm.test('Status code is 200', function () {
+  pm.response.to.have.status(200);
+});
+
+pm.test('Post has a new comment', function () {
+  pm.expect(pm.response.text()).to.include(pm.environment.get('comment'));
+});
+
+pm.test('User is authorized', function () {
+  pm.expect(pm.response.text()).to.include('logged-in-as');
+});
